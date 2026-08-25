dfdb8c6c48c89c5c5260cd7ad54b14bdc8d75554
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 00:49:49 -0400

    Add DOCX to Markdown route

---
+++ app/docx-to-markdown/page.tsx
@@ -0,0 +1,2 @@
+import { DocumentSuitePage } from '@/components/DocumentSuitePage';
+export default function DocxToMarkdownPage() { return <DocumentSuitePage route="/docx-to-markdown" mode="docx-to-markdown" />; }
