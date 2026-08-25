5b65c5d1c8fecad406d0951e76628ccd4872509d
webdesigngta <webdesigngta2026@gmail.com>
2026-08-24 23:37:43 -0400

    Export PDF document utility processor

---
+++ tools/pdf/document/index.ts
@@ -0,0 +1,7 @@
+export {
+  PdfDocumentUtilityProcessor,
+  pdfDocumentUtilityProcessor,
+  type PdfDocumentUtilityOptions,
+  type PdfDocumentUtilityResult,
+  type PdfFormValue,
+} from './PdfDocumentUtilityProcessor';
