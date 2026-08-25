5b2e031b1eee6a16a891a6a68a324972f5ba2177
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 09:00:56 -0400

    Add Translate PDF route

---
+++ app/translate-pdf/page.tsx
@@ -0,0 +1,2 @@
+import { RoadmapRemainingPage } from '@/components/RoadmapRemainingPage';
+export default function Page(){return <RoadmapRemainingPage route="/translate-pdf" mode="translate-pdf"/>;}
