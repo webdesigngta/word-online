c4b8edcd0b0606110d4434bdd2370341ada1c429
webdesigngta <webdesigngta2026@gmail.com>
2026-08-24 23:47:41 -0400

    Export CSV to PDF processor

--- tools/spreadsheet/index.ts
+++ tools/spreadsheet/index.ts
@@ -1,4 +1,5 @@
 export { ExcelToPdfProcessor, excelToPdfProcessor, registerExcelToPdfProcessor } from './ExcelToPdfProcessor';
+export { CsvToPdfProcessor, csvToPdfProcessor, registerCsvToPdfProcessor, type CsvToPdfResult } from './CsvToPdfProcessor';
 export { excelToPdfTool } from './ExcelToPdfTool';
 export type { ExcelToPdfOptions } from './ExcelToPdfOptions';
 export type { ExcelToPdfResult, ExcelToPdfOutput } from './ExcelToPdfResult';
