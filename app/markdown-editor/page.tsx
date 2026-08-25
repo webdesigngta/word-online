e546cdadc3d40e8ffe308dd422903fcdd87c2b36
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 00:49:31 -0400

    Add Markdown editor route

---
+++ app/markdown-editor/page.tsx
@@ -0,0 +1,2 @@
+import { DocumentSuitePage } from '@/components/DocumentSuitePage';
+export default function MarkdownEditorPage() { return <DocumentSuitePage route="/markdown-editor" mode="markdown-editor" />; }
