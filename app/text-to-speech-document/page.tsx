51b060d40e78dee3ac65564c2c41840dd6628624
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 00:50:02 -0400

    Add text to speech document route

---
+++ app/text-to-speech-document/page.tsx
@@ -0,0 +1,2 @@
+import { DocumentSuitePage } from '@/components/DocumentSuitePage';
+export default function TextToSpeechDocumentPage() { return <DocumentSuitePage route="/text-to-speech-document" mode="text-to-speech-document" />; }
