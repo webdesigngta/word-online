d8537e324fcf9c9bd293a3133c75098723926e5f
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 01:22:30 -0400

    Add XLS viewer route

---
+++ app/xls-viewer/page.tsx
@@ -0,0 +1,5 @@
+import { SpreadsheetUtilityPage } from '@/components/SpreadsheetUtilityPage';
+
+export default function XlsViewerPage() {
+  return <SpreadsheetUtilityPage route="/xls-viewer" mode="xls-viewer" />;
+}
