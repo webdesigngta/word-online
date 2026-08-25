5a54be7d08f910263d87d7ea78a44b9ffc0943b0
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 01:26:13 -0400

    Add PDF to Excel route

---
+++ app/pdf-to-excel/page.tsx
@@ -0,0 +1,5 @@
+import { OfficeConversionPage } from '@/components/OfficeConversionPage';
+
+export default function PdfToExcelPage() {
+  return <OfficeConversionPage route="/pdf-to-excel" mode="pdf-to-excel" />;
+}
