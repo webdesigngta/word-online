ca59ab78b7a4a2ae2139a4c472f49d79060141ca
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 01:22:37 -0400

    Add Spreadsheet Online route

---
+++ app/spreadsheet-online/page.tsx
@@ -0,0 +1,5 @@
+import { SpreadsheetUtilityPage } from '@/components/SpreadsheetUtilityPage';
+
+export default function SpreadsheetOnlinePage() {
+  return <SpreadsheetUtilityPage route="/spreadsheet-online" mode="spreadsheet-online" />;
+}
