993b51451b04aeacaa9d9b4ab4431e8e2de72556
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 01:26:41 -0400

    Add PowerPoint Online route

---
+++ app/powerpoint-online/page.tsx
@@ -0,0 +1,5 @@
+import { PresentationBuilderPage } from '@/components/PresentationBuilderPage';
+
+export default function PowerPointOnlinePage() {
+  return <PresentationBuilderPage route="/powerpoint-online" mode="powerpoint-online" />;
+}
