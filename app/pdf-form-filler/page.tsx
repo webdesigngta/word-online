a47f47c3078b77a8fdb31d27d9ad0d21220b63e6
webdesigngta <webdesigngta2026@gmail.com>
2026-08-24 23:38:55 -0400

    Add PDF Form Filler route

---
+++ app/pdf-form-filler/page.tsx
@@ -0,0 +1,5 @@
+import { PdfDocumentUtilityPage } from '@/components/PdfDocumentUtilityPage';
+
+export default function PdfFormFillerPage() {
+  return <PdfDocumentUtilityPage route="/pdf-form-filler" mode="pdf-form-filler" />;
+}
