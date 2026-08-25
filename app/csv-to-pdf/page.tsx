bf2e238200f4392deb846d5ddc5ccbd774e01da3
webdesigngta <webdesigngta2026@gmail.com>
2026-08-24 23:48:56 -0400

    Add CSV to PDF route

---
+++ app/csv-to-pdf/page.tsx
@@ -0,0 +1,5 @@
+import { ScanCsvToPdfPage } from '@/components/ScanCsvToPdfPage';
+
+export default function CsvToPdfPage() {
+  return <ScanCsvToPdfPage route="/csv-to-pdf" mode="csv-to-pdf" />;
+}
