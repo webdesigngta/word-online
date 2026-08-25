08c078638e72b8ad935cc12fbceed3d7aa9e04fd
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 07:52:29 -0400

    Add Word watermark route

---
+++ app/add-watermark-word/page.tsx
@@ -0,0 +1,5 @@
+import { WordFinishPage } from '@/components/WordFinishPage';
+
+export default function AddWatermarkWordPage() {
+  return <WordFinishPage route="/add-watermark-word" mode="watermark" />;
+}
