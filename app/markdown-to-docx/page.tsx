c0d4656dcf26faeda057218821e34501738db173
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 00:49:42 -0400

    Add Markdown to DOCX route

---
+++ app/markdown-to-docx/page.tsx
@@ -0,0 +1,2 @@
+import { DocumentSuitePage } from '@/components/DocumentSuitePage';
+export default function MarkdownToDocxPage() { return <DocumentSuitePage route="/markdown-to-docx" mode="markdown-to-docx" />; }
