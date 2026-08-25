7c3863c4579b3b3546764712efdc06d4539dd149
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 07:52:40 -0400

    Add Word redaction route

---
+++ app/redact-word-document/page.tsx
@@ -0,0 +1,5 @@
+import { WordFinishPage } from '@/components/WordFinishPage';
+
+export default function RedactWordDocumentPage() {
+  return <WordFinishPage route="/redact-word-document" mode="redact" />;
+}
