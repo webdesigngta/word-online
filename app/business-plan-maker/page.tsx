b6cfced94e90a0d7d011ec90e671111891b898a9
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 01:05:18 -0400

    Add business plan maker route

---
+++ app/business-plan-maker/page.tsx
@@ -0,0 +1,2 @@
+import { CreatorSuitePage } from '@/components/CreatorSuitePage';
+export default function Page(){return <CreatorSuitePage route="/business-plan-maker" mode="business-plan-maker"/>;}
