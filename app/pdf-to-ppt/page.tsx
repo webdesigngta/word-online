39d9f7268718fcf3941805974e4792ca4ca1817a
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 01:26:20 -0400

    Add PDF to PowerPoint route

---
+++ app/pdf-to-ppt/page.tsx
@@ -0,0 +1,5 @@
+import { OfficeConversionPage } from '@/components/OfficeConversionPage';
+
+export default function PdfToPptPage() {
+  return <OfficeConversionPage route="/pdf-to-ppt" mode="pdf-to-ppt" />;
+}
