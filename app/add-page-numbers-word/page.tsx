1d450554f6cc1874874f52ba27fe3f168ba4ed71
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 07:52:16 -0400

    Add Word page numbers route

---
+++ app/add-page-numbers-word/page.tsx
@@ -0,0 +1,5 @@
+import { WordFinishPage } from '@/components/WordFinishPage';
+
+export default function AddPageNumbersWordPage() {
+  return <WordFinishPage route="/add-page-numbers-word" mode="page-numbers" />;
+}
