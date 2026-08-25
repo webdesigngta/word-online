4203471e91748085def97a6b1fb6f5018bcc22af
webdesigngta <webdesigngta2026@gmail.com>
2026-08-24 23:24:58 -0400

    Add PDF Annotator route

---
+++ app/pdf-annotator/page.tsx
@@ -0,0 +1,5 @@
+import { PdfMarkupPage } from '@/components/PdfMarkupPage';
+
+export default function PdfAnnotatorPage() {
+  return <PdfMarkupPage route="/pdf-annotator" mode="pdf-annotator" />;
+}
