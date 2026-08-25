43a82af1fd6e29a56f01d84cb3f43d4201cefd72
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 09:00:29 -0400

    Add protect Word route

---
+++ app/protect-word-document/page.tsx
@@ -0,0 +1,2 @@
+import { RoadmapRemainingPage } from '@/components/RoadmapRemainingPage';
+export default function Page(){return <RoadmapRemainingPage route="/protect-word-document" mode="protect-word-document"/>;}
