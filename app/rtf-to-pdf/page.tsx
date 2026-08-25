7352d2ad8d4cb387a5018cd475129502246d9b30
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 00:14:05 -0400

    Add RTF to PDF route

---
+++ app/rtf-to-pdf/page.tsx
@@ -0,0 +1,5 @@
+import { DocumentFormatPage } from '@/components/DocumentFormatPage';
+
+export default function RtfToPdfPage() {
+  return <DocumentFormatPage route="/rtf-to-pdf" mode="rtf-to-pdf" />;
+}
