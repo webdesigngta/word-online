83cfbc8bdb16ca02f7c92039f6d0d33ff080db56
webdesigngta <webdesigngta2026@gmail.com>
2026-08-25 01:05:00 -0400

    Add meeting minutes maker route

---
+++ app/meeting-minutes-maker/page.tsx
@@ -0,0 +1,2 @@
+import { CreatorSuitePage } from '@/components/CreatorSuitePage';
+export default function Page(){return <CreatorSuitePage route="/meeting-minutes-maker" mode="meeting-minutes-maker"/>;}
