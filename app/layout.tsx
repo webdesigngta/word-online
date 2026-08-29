464efa2c3ee12916997b47a9b8d7d5a06d7c5766
webdesigngta <webdesigngta2026@gmail.com>
2026-08-29 14:11:12 -0400

    Load homepage search stacking fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ import './text-tool-uniformity.css';
 import './docx-to-html-compact.css';
 import './home-typography.css';
 import './home-readability.css';
+import './home-search-layer.css';
 import { ScrollToTop } from '@/components/ScrollToTop';
 import { absoluteUrl, allowIndexing, site, siteUrl } from '@/lib/site';
 
